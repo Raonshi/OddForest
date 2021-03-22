2abd93d09af088693f1df6eeb364bfce150ec334
Raonshi <56419380+Raonshi@users.noreply.github.com>
2021-03-22 16:53:22 +0900

    Commit : 배경 이미지 작업 중
    1. 배경 이미지가 자꾸 깨짐
    2. 스프라이트 조정이 필요할 듯.

---
+++ OddForest/Assets/Scripts/Camera/CameraControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+
+    public Transform target;
+    public float up;
+    public float zoom;
+
+    public GameObject cameraCollider;
+
+    public float cameraMoveSpeed;
+
+    public GameObject pos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (pos.transform.position.x <= GameObject.Find("CameraWall_Mid").transform.position.x ||
+            pos.transform.position.x >= GameObject.Find("CameraWall_Right").transform.position.x)
+        {
+            return;
+        }
+        
+        Vector3 targetPos = new Vector3(target.position.x, target.position.y + up, zoom);
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, cameraMoveSpeed * Time.deltaTime);
+    }
+}
