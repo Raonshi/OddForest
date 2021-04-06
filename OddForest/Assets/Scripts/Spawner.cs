857e4dfbcfd2d0f3b8ef51ece666690e513a2dc1
Raonshi <56419380+Raonshi@users.noreply.github.com>
2021-04-06 15:13:11 +0900

    Commit : 적 스폰 기능 구현
    이제 적 유닛마다 애니메이션 작업 해야함

--- OddForest/Assets/Scripts/Spawner.cs
+++ OddForest/Assets/Scripts/Spawner.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public float spawnTime;
+    public float delta;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        delta = spawnTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        delta -= Time.deltaTime;
+
+        if (delta <= 0)
+        {
+            //소환
+            SpawnEnemy();
+            delta = spawnTime;
+        }
+    }
+
+    public void SpawnEnemy()
+    {
+        int rand = Random.Range(0, 4);
+
+        GameObject obj = new GameObject();
+        obj.AddComponent<Enemy>();
+        obj.AddComponent<SpriteRenderer>();
+
+        Enemy enemy = obj.GetComponent<Enemy>();
+        enemy.name = (Enemy.Name)rand;
+
+        obj.name = enemy.name.ToString();
+        obj.transform.SetParent(this.transform);
+        obj.transform.position = this.transform.position;
+
+        enemy.Init();
     }
 }
---
+++ OddForest/Assets/Scripts/Unit/Enemy.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public enum Name
+    {
+        Wolf,
+        Bat,
+        Vampire,
+        Orge,
+    }
+
+    public Name name;
+
+    public int hp;
+    public int atk;
+    public float moveSpeed;
+
+    public SpriteRenderer sprite;
+
+
+    public void Init()
+    {
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+
+        Sprite[] sp;
+
+        switch (name)
+        {
+            case Name.Wolf:
+                hp = 20;
+                atk = 10;
+                moveSpeed = 2;
+
+                sp = Resources.LoadAll<Sprite>("Images/Units/Wolf/Idle");
+                sprite.sprite = sp[0];
+
+                //sprite.sprite = Resources.Load<Sprite>("Images/Units/Wolf/Idle_0");
+
+                break;
+            case Name.Bat:
+                hp = 10;
+                atk = 10;
+                moveSpeed = 3;
+
+                sp = Resources.LoadAll<Sprite>("Images/Units/Bat/Fly");
+                sprite.sprite = sp[0];
+
+                //sprite.sprite = Resources.Load<Sprite>("Images/Units/Bat/Fly_0");
+
+                break;
+            case Name.Vampire:
+                hp = 150;
+                atk = 25;
+                moveSpeed = 4;
+
+                sp = Resources.LoadAll<Sprite>("Images/Units/Vampire/Idle");
+                sprite.sprite = sp[0];
+
+                //sprite.sprite = Resources.Load<Sprite>("Images/Units/Vampire/Idle_0");
+
+                break;
+            case Name.Orge:
+                hp = 300;
+                atk = 60;
+                moveSpeed = 2;
+
+                sp = Resources.LoadAll<Sprite>("Images/Units/Orge/Idle");
+                sprite.sprite = sp[0];
+
+                //sprite.sprite = Resources.Load<Sprite>("Images/Units/Orge/Idle_0");
+
+                break;
+        }
+    }
+}
