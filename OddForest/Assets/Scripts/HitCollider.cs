c22a5e794158f81c541b5e0c0bf383da09faa5b6
Raonshi <56419380+Raonshi@users.noreply.github.com>
2021-04-07 11:56:24 +0900

    Commit : 플레이어 공격 데미지 처리 작업 중

---
+++ OddForest/Assets/Scripts/HitCollider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitCollider : MonoBehaviour
+{
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            other.GetComponent<Enemy>().hp -= Player.instance.atk;
+        }
+    }
+}
