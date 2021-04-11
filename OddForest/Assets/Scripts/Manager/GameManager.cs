ee017d8e4b2a4b07a8f9d67c86ba8dcad6581be6
Raonshi <56419380+Raonshi@users.noreply.github.com>
2021-04-11 18:44:57 +0900

    Commit : 게임매니저, 세이브매니저, 타이틀
    1. 타이틀 애니매이션 작업
    2. 게임매니져를 통해 매초 네트워크 연결 확인
    3. 세이브 매니저를 통해 데이터 Json으로 저장

--- OddForest/Assets/Scripts/Manager/GameManager.cs
+++ OddForest/Assets/Scripts/Manager/GameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public bool isNew = true; //세이브 매니저로 데이터 저장하면 true값 없애야함
+    public float internetCheckTime;
 
 
     private static GameManager instance = null;
@@ -31,15 +32,21 @@ public class GameManager : MonoBehaviour
 
     public void Init()
     {
+        //기본 변수 셋팅
+        internetCheckTime = 1.0f;
+
         //저장 정보 불러와야함
 
         //신규유저일 경우 
         if(isNew == true)
         {
             //초기 정보 설정
+            Debug.Log("new Player!");
         }
 
         Debug.Log("GameManager Created!");
+
+        SaveManager.Singleton.Init();
     }
 
     // Start is called before the first frame update
@@ -51,6 +58,31 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //매초 인터넷 연결 체크
+        internetCheckTime -= Time.deltaTime;
+        if(internetCheckTime < 0)
+        {
+            CheckInternet();
+            internetCheckTime = 1.0f;
+        }
+    }
+
+    /// <summary>
+    /// 게임 앱이 실행되는 동안 인터넷 연결 상태를  체크한다.
+    /// </summary>
+    public void CheckInternet()
+    {
+        switch (Application.internetReachability)
+        {
+            //인터넷에 연결되어 있지 않을 경우
+            case NetworkReachability.NotReachable:
+                Debug.Log(string.Format("Please Connect Internet"));
+                break;
+            //인터넷에 연결되어 있을 경우
+            case NetworkReachability.ReachableViaCarrierDataNetwork:
+            case NetworkReachability.ReachableViaLocalAreaNetwork:
+                Debug.Log(string.Format("Internet Connected"));
+                break;
+        }
     }
 }
