ae5ad1f2705611242102ed9b5502399d1ce8d730
Raonshi <56419380+Raonshi@users.noreply.github.com>
2021-04-11 18:44:57 +0900

    Commit : 게임매니저, 세이브매니저, 타이틀
    1. 타이틀 애니매이션 작업
    2. 게임매니져를 통해 매초 네트워크 연결 확인
    3. 세이브 매니저를 통해 데이터 Json으로 저장

---
+++ OddForest/Assets/Scripts/Manager/SaveManager.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour
+{
+    private static SaveManager instance = null;
+
+    public static SaveManager Singleton
+    {
+        get
+        {
+            if (instance == null)
+            {
+                GameObject obj = GameObject.Find("SaveManager");
+                if(obj == null)
+                {
+                    obj = new GameObject("SaveManager");
+                    obj.AddComponent<SaveManager>();
+                    DontDestroyOnLoad(obj);
+                }
+                instance = obj.GetComponent<SaveManager>();
+            }
+            return instance;
+        }
+    }
+
+
+    public void Init()
+    {
+        Debug.Log("SaveManager has created");
+        SavePlayerData();
+
+        LoadPlayerData();
+    }
+
+    /// <summary>
+    /// 플레이어 데이터 저장
+    /// </summary>
+    public void SavePlayerData()
+    {
+        PlayerJson json = new PlayerJson();
+        json.SetData(1, 1, 1, 1, 1);
+
+        string data = json.Serealize();
+        string path = Application.persistentDataPath + "/Save/";
+
+        //저장 폴더가 존재하지 않을 경우
+        if(Directory.Exists(path) == false)
+        {
+            Directory.CreateDirectory(path);
+        }
+
+        //데이터 스트림 생성 및 데이터 저장
+        StreamWriter stream = new StreamWriter(path + "player.json");
+        stream.Write(data);
+        stream.Dispose();
+
+        Debug.Log("End!");
+    }
+
+
+    public void LoadPlayerData()
+    {
+        string path = Application.persistentDataPath + "/Save/";
+
+        if(Directory.Exists(path) == false)
+        {
+            Directory.CreateDirectory(path);
+        }
+
+        //파일로 저장된 json데이터를 string으로 불러옴
+        StreamReader stream = new StreamReader(path + "player.json");
+        string json = stream.ReadToEnd();
+        stream.Dispose();
+
+        PlayerJson data = JsonUtility.FromJson<PlayerJson>(json);
+
+        Debug.Log(string.Format("hp = {0}, atk = {1}, cri = {2}, gold = {3}, best = {4}", data.maxHp, data.atk, data.cri, data.gold, data.bestScore));
+    }
+}
+
+
+#region JSON Data
+
+public class PlayerJson
+{
+    public int maxHp;
+    public int atk;
+    public int cri;
+    public int gold;
+    public int bestScore;
+
+    public void SetData(int _maxHp, int _atk, int _cri, int _gold, int _bestScore)
+    {
+        maxHp = _maxHp;
+        atk = _atk;
+        cri = _cri;
+        gold = _gold;
+        bestScore = _bestScore;
+    }
+
+    public string Serealize()
+    {
+        return JsonUtility.ToJson(this);
+    }
+}
+
+#endregion
--- OddForest/Assets/Scripts/Title.cs
+++ OddForest/Assets/Scripts/Title.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Title : MonoBehaviour
 {
+
+    public static Title instance = null;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        instance = this;
+
+        GameManager.Singleton.Init();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //esc버튼(안드로이드 뒤로가기 버튼)터치 시 게임 종료
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+
+    public void OnClickGameStart()
+    {
+        SceneManager.LoadScene("Main");
     }
 }
