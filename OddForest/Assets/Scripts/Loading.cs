275ccf23bd14837af919c4361e42e708931fb06e
Raonshi <56419380+Raonshi@users.noreply.github.com>
2021-04-14 11:14:18 +0900

    Commit : 로딩 화면 구현 끝

---
+++ OddForest/Assets/Scripts/Loading.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Loading : MonoBehaviour
+{
+    public Text loading;
+    public string message;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        message = "L O A D I N G . . .";
+
+        StartCoroutine(LoadNextScene());
+    }
+
+    IEnumerator LoadNextScene()
+    {
+        yield return new WaitForSeconds(1.0f);
+
+        AsyncOperation async = SceneManager.LoadSceneAsync(GameManager.Singleton.nextScene);
+        async.allowSceneActivation = false;
+
+        while(async.isDone == false)
+        {
+            //로딩 진행 중
+            if(async.progress < 0.9f)
+            {
+                loading.text = message;
+            }
+            else
+            {
+                async.allowSceneActivation = true;
+                yield break;
+            }
+        }
+    }
+}
